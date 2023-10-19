3a9e76b997551d525bd869d92b91e8c723dff33f
jontsbarbosa <jonatas.barbosa@live.com>
2023-10-19 08:35:13 -0300

    Projeto de diagramação de classes

---
+++ README.md
@@ -0,0 +1,8 @@
+# Desafio de Projeto - Diagramação de classes do Iphone
+Repositório criado para o Desafio de Projeto, utilizando a linguagem Java. 
+
+## TODO
+Diagramação utilizando UML
+
+## Links Úteis
+[Sintaxe Básica Markdown](https://www.markdownguide.org/basic-syntax/)
---
+++ src/Iphone/Iphone.java
@@ -0,0 +1,31 @@
+package Iphone;
+
+public class Iphone implements ReprodutorMusical, AparelhoEletronico, NavegadorInternet {
+
+    public void ligar(String numero) {
+    }
+
+    public void atender(String numero) {
+    }
+    
+    public void iniciarCorreioVoz(String mensagem, String destinatario) {
+    }
+
+    public void exibirPagina(String url) {
+    }
+
+    public void adicionarNovaAba(String url) {
+    }
+
+    public void atualizarPagina() {
+    }
+
+    public void tocar() {
+    }
+
+    public void pausar() {
+    }
+
+    public void selecionarMusica() {
+    }
+}
